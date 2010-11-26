20aa967433f455eb21ea2c6d256d4d04064897d0
Till Claassen <code@pixelistik.de>
2010-11-26 17:48:10 +0100

    noch mehr wiederholungen

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while [ 1 ]
 do
-	cat archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal>compiledsignal
+	cat archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal>compiledsignal
 	#cat livesignal>compiledsignal
 done
