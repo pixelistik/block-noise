55a913af34e0fbe9f6774f9f98eacfd38b98d9c4
Till Claassen <code@pixelistik.de>
2011-02-13 18:25:52 +0100

    Fullscreen aus, zum testen besser

--- mosh.sh
+++ mosh.sh
@@ -1,3 +1,3 @@
 #cat archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal archivesignal livesignal|ffmpeg -i - -aspect 4:3 -f avi -an -vcodec libxvid -g 200 -vb 2500000 -s 360x288 -r 25 -y -|../autodatamosh/autodatamosh.pl -dprob 0 -dmin 4 -dmax 60|cvlc -
-cat compiledsignal|ffmpeg -i - -aspect 4:3 -f avi -an -vcodec libxvid -g 200 -vb 2500000 -s 720x576 -deinterlace -r 25 -y -|../autodatamosh/autodatamosh.pl -dprob 0.1 -dmin 4 -dmax 60|cvlc --fullscreen --no-overlay -
+cat compiledsignal|ffmpeg -i - -aspect 4:3 -f avi -an -vcodec libxvid -g 200 -vb 2500000 -s 720x576 -deinterlace -r 25 -y -|../autodatamosh/autodatamosh.pl -dprob 0.1 -dmin 4 -dmax 60|cvlc --no-overlay -
 #cat compiledsignal|../autodatamosh/autodatamosh.pl -dprob 0 -dmin 4 -dmax 60|cvlc -
