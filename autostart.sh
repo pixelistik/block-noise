6d9419ed8b627ae0992062670fefd58204029830
Till Claassen <code@pixelistik.de>
2011-07-04 22:47:29 +0200

    Auch watchdog autstarten

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Dieses Script startet alle anderen. Normalerweise muss nur dieses Script ausgeführt werden.
+
 ./resetarchive.sh
 sleep 2
 ./archivefeed.sh &
@@ -8,3 +9,5 @@ sleep 3
 ./compile.sh &
 sleep 2
 ./mosh.sh &
+sleep 8
+./watchdog.sh &
