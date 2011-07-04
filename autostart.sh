c4b2e0a34be805d36ef72f2ae6336270a9d3364d
Till Claassen <code@pixelistik.de>
2011-07-04 22:44:30 +0200

    Autostart-Datei, um mit einem Befehl alles starten zu können

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Dieses Script startet alle anderen. Normalerweise muss nur dieses Script ausgeführt werden.
+./resetarchive.sh
+sleep 2
+./archivefeed.sh &
+./livefeed.sh &
+sleep 3
+./compile.sh &
+sleep 2
+./mosh.sh &
