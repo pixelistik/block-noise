df38dbedd3668113707d85e28f368345e9b707c5
Till Claassen <code@pixelistik.de>
2011-07-05 16:59:34 +0200

    Brutales stopall -> sehr viele killall-Befehle...

--- autostart.sh
+++ autostart.sh
@@ -4,10 +4,15 @@
 ./resetarchive.sh
 sleep 2
 ./archivefeed.sh &
+#echo $!>/tmp/archivefeed.pid
 ./livefeed.sh &
+#echo $!>/tmp/livefeed.pid
 sleep 3
 ./compile.sh &
+#echo $!>/tmp/compile.pid
 sleep 2
 ./mosh.sh &
+#echo $!>/tmp/mosh.pid
 sleep 8
 ./watchdog.sh &
+#echo $!>/tmp/watchdog.pid
---
+++ stopall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#kill $(echo /tmp/archivefeed.pid)
+#kill $(echo /tmp/livefeed.pid)
+#kill $(echo /tmp/compile.pid)
+#kill $(echo /tmp/mosh.pid)
+#kill $(echo /tmp/watchdog.pid)
+
+#rm /tmp/archivefeed.pid
+#rm /tmp/livefeed.pid
+#rm /tmp/compile.pid
+#rm /tmp/mosh.pid
+#rm /tmp/watchdog.pid
+
+killall archivefeed.sh
+killall livefeed.sh
+killall compile.sh
+killall mosh.sh
+killall watchdog.sh
+
+killall vlc
+killall ffmpeg
+killall dvgrab
