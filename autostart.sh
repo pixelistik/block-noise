bfd172e57dc9be15e8e77b949299295f1b0781fe
Till Claassen <code@pixelistik.de>
2012-01-19 18:24:07 +0100

    Startnachricht mit Erläuterung

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # Dieses Script startet alle anderen. Normalerweise muss nur dieses Script ausgeführt werden.
+
+echo "======================================="
+echo "=            BLOCK NOISE!             ="
+echo "======================================="
+echo
+echo "Die Videoinstallation wird jetzt gestartet"
+echo
+echo "Zum Stoppen:"
+echo "Drücke ESC und klicke dann auf den Stop-Button."
+echo
+sleep 4
+
 echo "`date` Autostart">>protokoll.log
 ./resetarchive.sh
 sleep 2
